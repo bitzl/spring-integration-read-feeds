74ea916c60e3e075418a710071adcb49e546adaf
Marcus Bitzl <marcus@bitzl.com>
2016-03-23 00:10:38 +0100

    Fetch 100 messages per poll.

--- src/main/java/com/bitzl/examples/spring/integration/feedreader/integration/config/FeedReadingConfig.java
+++ src/main/java/com/bitzl/examples/spring/integration/feedreader/integration/config/FeedReadingConfig.java
@@ -7,6 +7,7 @@ import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.dsl.feed.Feed;
 import org.springframework.integration.scheduling.PollerMetadata;
 
 import java.net.MalformedURLException;
@@ -18,14 +19,14 @@ public class FeedReadingConfig {
 
     @Bean(name = PollerMetadata.DEFAULT_POLLER)
     public PollerMetadata poller() {
-        return Pollers.fixedRate(1000).get();
+        return Pollers.fixedRate(1000).maxMessagesPerPoll(100).get();
     }
 
     @Bean
     public IntegrationFlow feedReadingFlow() throws MalformedURLException {
         URL url = new URL("http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/world/rss.xml");
         return IntegrationFlows
-                .from((IntegrationFlows.MessageSourcesFunction) messageSources -> messageSources.feed(url, "BBC Key"))
+                .from(Feed.inboundAdapter(url, "BBC"))
                 .transform(new SummaryTransformer())
                 .handle(System.out::println)
                 .get();
